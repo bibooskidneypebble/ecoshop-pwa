7593851a3a2694f507110d2556a0e9ae102e2f2c
bibooskidneypebble <abacaxibaxinho@gmail.com>
2026-04-10 09:18:37 -0300

    fix cache paths

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = 'ecoshop-v1';
 
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
